17ad2492bcc5a95da4da79ab40b34525cc0e0a4d
Randy Sargent <rsargent@trinidad.local>
2010-08-01 11:25:09 -0400

    Initial commit

---
+++ src/org/bodytrack/client/Grapher2.java
@@ -0,0 +1,24 @@
+package org.bodytrack.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class Grapher2 implements EntryPoint {
+	private VerticalPanel mainLayout = new VerticalPanel(); 
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+		Label label = new Label();
+		label.setText("hi");
+		
+		mainLayout.add(label);
+
+		RootPanel.get("graph").add(mainLayout);
+	}
+}
