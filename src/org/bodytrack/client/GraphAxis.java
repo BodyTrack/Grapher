bda13155b464654a01c5b0327dd3309bbd4b94f3
Adam Mihalcin <adammih@gmail.com>
2010-10-09 21:24:24 -0400

    Updated src/org/bodytrack/Grapher2.gwt.xml to allow AJAX connections.
    Added a rule to Grapher2.gwt.xml to allow AJAX connections back to the
    server.
    
    Added more rules to the .gitignore, making it similar to the .gitignore
    currently (commit 79b6d2f) in the widgets repository.  Changed Java
    files to follow the 80-character rule more closely.

--- src/org/bodytrack/client/GraphAxis.java
+++ src/org/bodytrack/client/GraphAxis.java
@@ -1,227 +1,286 @@
 package org.bodytrack.client;
 
 
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.i18n.client.NumberFormat;
 import gwt.g2d.client.graphics.DirectShapeRenderer;
 import gwt.g2d.client.graphics.Surface;
 import gwt.g2d.client.graphics.TextAlign;
 import gwt.g2d.client.graphics.TextBaseline;
 import gwt.g2d.client.math.Vector2;
 
+import com.google.gwt.i18n.client.NumberFormat;
+
 //import java.util.Date;
 // TODO: implement date
 public class GraphAxis {
 	public double majorTickMinSpacingPixels = 50;
-    public double majorTickWidthPixels = 8;
+	public double majorTickWidthPixels = 8;
 
-    public double minorTickMinSpacingPixels = 10;
-    public double minorTickWidthPixels = 3;
+	public double minorTickMinSpacingPixels = 10;
+	public double minorTickWidthPixels = 3;
 
 	protected double min;
 	protected double max;
-	
+
 	protected boolean hasMinRange = false;
 	protected double minRange = 1e-100;
-	
+
 	protected boolean hasMaxRange = false;
 	protected double maxRange = 1e+100;
-	
+
 	protected Basis basis;
 	private Vector2 begin;
 	private double width;
 	protected double length;
 	private double scale;
 	private BBox bounds;
-	
+
 	GraphAxis(double min, double max, Basis basis, double width) {
 		this.min = min;
 		this.max = max;
 		this.basis = basis;
 		this.width = width;
 	}
-	
+
 	public void layout(Vector2 begin, double length) {
 		this.begin = begin;
 		this.length = length;
 		rescale();
 	}
-	
+
 	private void rescale() {
 		this.scale = length / (this.max - this.min);
 		Vector2 vWidth = new Vector2(basis.x.scale(this.width));
 		Vector2 vLength = new Vector2(basis.y.scale(this.length));
 		Vector2 end = this.begin.add(vWidth).add(vLength);
-		bounds = new BBox(new Vector2(Math.min(begin.getX(), end.getX()),Math.min(begin.getY(), end.getY())),
-				          new Vector2(Math.max(begin.getX(), end.getX()),Math.max(begin.getY(), end.getY())));				
+		bounds = new BBox(new Vector2(Math.min(begin.getX(), end.getX()),
+				Math.min(begin.getY(), end.getY())),
+				new Vector2(Math.max(begin.getX(), end.getX()),
+						Math.max(begin.getY(), end.getY())));
 	}
-	
+
 	public double project1D(double value) {
 		return (value-this.min) * scale;
 	}
+
 	public Vector2 project2D(double value) {
 		return begin.add(basis.y.scale(project1D(value)));
 	}
+
 	public double unproject(Vector2 point) {
 		return this.min + (point.subtract(begin).dot(basis.y) / scale);
 	}
-	
+
 	public void paint(Surface surface) {
 		DirectShapeRenderer renderer = new DirectShapeRenderer(surface);
-	    renderer.beginPath();
+		renderer.beginPath();
 		renderer.drawLineSegment(project2D(this.min), project2D(this.max));
-		
 
-	    double majorTickSize = computeTickSize(majorTickMinSpacingPixels);
-	    renderTicks(0, majorTickSize, null, surface, renderer, majorTickWidthPixels, new DefaultLabelFormatter());
-	    //renderTickLabels(surface, majorTickSize, majorTickWidthPixels+3);
+		double majorTickSize = computeTickSize(majorTickMinSpacingPixels);
+		renderTicks(0, majorTickSize, null, surface, renderer,
+				majorTickWidthPixels, new DefaultLabelFormatter());
+		//renderTickLabels(surface, majorTickSize, majorTickWidthPixels+3);
 
-	    double minorTickSize = computeTickSize(minorTickMinSpacingPixels);
-	    renderTicks(0, minorTickSize, null, surface, renderer, minorTickWidthPixels, null);
-	    	
+		double minorTickSize = computeTickSize(minorTickMinSpacingPixels);
+		renderTicks(0, minorTickSize, null, surface, renderer,
+				minorTickWidthPixels, null);
+
 		renderer.stroke();
 	}
-	
+
 	abstract class LabelFormatter {
 		abstract String format(double value);
 	}
 
 	class DefaultLabelFormatter extends LabelFormatter {
 		String format(double value) {
-	       String label = NumberFormat.getDecimalFormat().format(value);
-	        if (label.equals("-0")) label="0";
-	        return label;
+			String label = NumberFormat.getDecimalFormat().format(value);
+			if (label.equals("-0")) label="0";
+			return label;
 		}
 	}
-	
+
 	class TickGenerator {
 		private double tickSize;
 		private double offset;
 		private double currentTick = 0.0;
-		
+
 		TickGenerator(double tickSize, double offset) {
 			this.tickSize = tickSize;
 			this.offset = offset;
 		}
+
 		double nextTick(double min) {
 			currentTick = closestTick(min - tickSize);
 			while (currentTick < min) advanceTick();
 			return currentTick;
 		}
+
 		double nextTick() {
 			advanceTick();
 			return currentTick;
 		}
+
 		void advanceTick() {
 			currentTick = closestTick(currentTick + tickSize);
 		}
+
 		double closestTick(double val) {
-			return Math.round((val-offset) / tickSize) * tickSize + offset; 
+			return Math.round((val - offset) / tickSize) * tickSize + offset;
 		}
 	}
 
 	final int JUSTIFY_MIN = 0;
 	final int JUSTIFY_MED = 1;
 	final int JUSTIFY_MAX = 2;
-	
+
 	protected double setupText(Surface surface, int justify) {
-	    boolean textParallelToAxis = (Math.abs(basis.x.getX()) < Math.abs(basis.x.getY()));
-	    double labelOffsetPixels = 0;
-	    if (textParallelToAxis) {
-	    	final TextAlign align[]={TextAlign.LEFT, TextAlign.CENTER, TextAlign.RIGHT};
-	    	surface.setTextAlign(align[justify]);
-	    	surface.setTextBaseline(TextBaseline.TOP);
-	    } else {
-	        surface.setTextAlign(TextAlign.LEFT);
-	        final TextBaseline baseline[]={TextBaseline.BOTTOM, TextBaseline.MIDDLE, TextBaseline.TOP};
-	        surface.setTextBaseline(baseline[justify]);
-	        labelOffsetPixels = 3;
-	    }
+		boolean textParallelToAxis =
+			(Math.abs(basis.x.getX()) < Math.abs(basis.x.getY()));
+		double labelOffsetPixels = 0;
+
+		if (textParallelToAxis) {
+			final TextAlign[] align =
+			{TextAlign.LEFT, TextAlign.CENTER, TextAlign.RIGHT};
+
+			surface.setTextAlign(align[justify]);
+			surface.setTextBaseline(TextBaseline.TOP);
+		} else {
+			surface.setTextAlign(TextAlign.LEFT);
+
+			final TextBaseline[] baseline =
+			{TextBaseline.BOTTOM, TextBaseline.MIDDLE, TextBaseline.TOP};
+
+			surface.setTextBaseline(baseline[justify]);
+			labelOffsetPixels = 3;
+		}
+
 		return labelOffsetPixels;
 	}
 
-	protected void renderTicks(double offsetPixels, double tickSize, TickGenerator tickGen, Surface surface, DirectShapeRenderer renderer, double tickWidthPixels, LabelFormatter formatter) {
+	protected void renderTicks(double offsetPixels, double tickSize,
+			TickGenerator tickGen, Surface surface,
+			DirectShapeRenderer renderer, double tickWidthPixels,
+			LabelFormatter formatter) {
 		if (tickGen == null) tickGen = new TickGenerator(tickSize, 0);
-		
-		double labelOffsetPixels = formatter == null ? 0 : setupText(surface, JUSTIFY_MED) + offsetPixels + tickWidthPixels;
-		
+
+		double labelOffsetPixels = formatter == null ? 0
+				: setupText(surface, JUSTIFY_MED) + offsetPixels + tickWidthPixels;
+
 		for (double tick = tickGen.nextTick(this.min); tick <= this.max; tick = tickGen.nextTick()) {
 			renderTick(renderer, tick, offsetPixels+tickWidthPixels);
-			if (formatter != null) renderTickLabel(surface, tick, labelOffsetPixels, formatter);
+			if (formatter != null)
+				renderTickLabel(surface, tick, labelOffsetPixels, formatter);
 		}
 	}
-	
-	protected void renderTicksRangeLabelInline(double offsetPixels, double tickSize, TickGenerator tickGen, Surface surface, DirectShapeRenderer renderer, double tickWidthPixels, LabelFormatter formatter) {
-		if (tickGen == null) tickGen = new TickGenerator(tickSize, 0);
-		
-		double labelOffsetPixels = setupText(surface, JUSTIFY_MED) + offsetPixels;
-		
+
+	protected void renderTicksRangeLabelInline(double offsetPixels,
+			double tickSize, TickGenerator tickGen, Surface surface,
+			DirectShapeRenderer renderer, double tickWidthPixels,
+			LabelFormatter formatter) {
+		if (tickGen == null)
+			tickGen = new TickGenerator(tickSize, 0);
+
+		double labelOffsetPixels = setupText(surface, JUSTIFY_MED)
+		+ offsetPixels;
+
 		double tick = tickGen.nextTick(this.min);
-		
+
 		if (tick > this.max) {
 			// No ticks are visible
 			// Draw one inline label in the middle
-			renderTickLabel(surface, (this.min + this.max) / 2., labelOffsetPixels, formatter);
+			renderTickLabel(surface, (this.min + this.max) / 2.0,
+					labelOffsetPixels, formatter);
 			return;
 		}
-		
+
 		int pixelMargin = 100;  // TODO: fix this
 		if (project1D(tick) >= pixelMargin) {
-			// Draw label for before first tick, justified to the minimum of axis (left or bottom)
+			// Draw label for before first tick, justified to the minimum
+			// of axis (left or bottom)
 			setupText(surface, JUSTIFY_MIN);
 			renderTickLabel(surface, this.min, labelOffsetPixels, formatter);
 			setupText(surface, JUSTIFY_MED);
 		}
-		
+
 		while (true) {
 			renderTick(renderer, tick, tickWidthPixels+offsetPixels);
 			double nextTick = tickGen.nextTick();
+
 			if (nextTick > this.max) break;
-			renderTickLabel(surface, (tick + nextTick) / 2., labelOffsetPixels, formatter);
+
+			renderTickLabel(surface, (tick + nextTick) / 2.0,
+					labelOffsetPixels, formatter);
 			tick = nextTick;
 		}
+
 		if (length - project1D(tick) >= pixelMargin) {
-			// Draw label for after last tick, justified to maximum of axis (right or top)
+			// Draw label for after last tick, justified to maximum of
+			// axis (right or top)
+
 			setupText(surface, JUSTIFY_MAX);
 			renderTickLabel(surface, this.max, labelOffsetPixels, formatter);
 		}
 	}
-	
-	protected void renderTicksRangeLabel(double offsetPixels, double tickSize, TickGenerator tickGen, TickGenerator endOfRangeGenerator, Surface surface, DirectShapeRenderer renderer, double tickWidthPixels, LabelFormatter formatter) {
-		if (tickGen == null) tickGen = new TickGenerator(tickSize, 0);
-		
-		double labelOffsetPixels = setupText(surface, JUSTIFY_MED) + offsetPixels + tickWidthPixels;
-		
+
+	protected void renderTicksRangeLabel(double offsetPixels, double tickSize,
+			TickGenerator tickGen, TickGenerator endOfRangeGenerator,
+			Surface surface, DirectShapeRenderer renderer,
+			double tickWidthPixels, LabelFormatter formatter) {
+		if (tickGen == null)
+			tickGen = new TickGenerator(tickSize, 0);
+
+		double labelOffsetPixels = setupText(surface, JUSTIFY_MED)
+		+ offsetPixels + tickWidthPixels;
+
 		double minTick = tickGen.nextTick(this.min-tickSize*1.5);
-		
+
 		while (minTick <= this.max) {
-			if (this.min <= minTick) renderTick(renderer, minTick, tickWidthPixels + offsetPixels);
+			if (this.min <= minTick)
+				renderTick(renderer, minTick, tickWidthPixels + offsetPixels);
+
 			endOfRangeGenerator.nextTick(minTick);
 			double maxTick = endOfRangeGenerator.nextTick();
-			if (this.min <= maxTick && maxTick <= this.max) renderTick(renderer, maxTick, tickWidthPixels + offsetPixels);
-			if (this.min <= minTick && maxTick <= this.max) renderTickLabel(surface, (minTick+maxTick)/2., labelOffsetPixels, formatter);
+
+			if (this.min <= maxTick && maxTick <= this.max)
+				renderTick(renderer, maxTick, tickWidthPixels + offsetPixels);
+
+			if (this.min <= minTick && maxTick <= this.max)
+				renderTickLabel(surface, (minTick+maxTick)/2.0,
+						labelOffsetPixels, formatter);
+
 			minTick = tickGen.nextTick();
 		}
 	}
-	
-	protected void renderTick(DirectShapeRenderer renderer, double tick, double tickWidthPixels) {
-		renderer.drawLineSegment(project2D(tick),	project2D(tick).add(this.basis.x.scale(tickWidthPixels)));
-	}
-	
-	protected void renderTickLabel(Surface surface, double tick, double labelOffsetPixels, LabelFormatter formatter) {
-		renderTickLabel(surface, tick, labelOffsetPixels, formatter.format(tick));
+
+	protected void renderTick(DirectShapeRenderer renderer, double tick,
+			double tickWidthPixels) {
+		renderer.drawLineSegment(project2D(tick),
+				project2D(tick).add(this.basis.x.scale(tickWidthPixels)));
 	}
 
-	protected void renderTickLabel(Surface surface, double y, double labelOffsetPixels, String label) {
+	protected void renderTickLabel(Surface surface, double tick,
+			double labelOffsetPixels, LabelFormatter formatter) {
+		renderTickLabel(surface, tick, labelOffsetPixels,
+				formatter.format(tick));
+	}
+
+	protected void renderTickLabel(Surface surface, double y,
+			double labelOffsetPixels, String label) {
 		// .setFont("italic 30px sans-serif")
-        surface.fillText(label, project2D(y).add(this.basis.x.scale(labelOffsetPixels)));
+		surface.fillText(label,
+				project2D(y).add(this.basis.x.scale(labelOffsetPixels)));
 	}
 
-	public double computeTickSize(double minPixels) { return computeTickSize(minPixels, 1.0); }
-	
+	public double computeTickSize(double minPixels) {
+		return computeTickSize(minPixels, 1.0);
+	}
+
 	public double computeTickSize(double minPixels, double unitSize) {
-		double minDelta = (this.max-this.min) * (minPixels/this.length) / unitSize;
-		double minDeltaMantissa = minDelta / Math.pow(10, Math.floor(Math.log10(minDelta)));
+		double minDelta = (this.max - this.min)
+		* (minPixels / this.length) / unitSize;
+		double minDeltaMantissa = minDelta
+		/ Math.pow(10, Math.floor(Math.log10(minDelta)));
+
 		// Round minDelta up to nearest (1,2,5)
 		double actualDeltaMantissa;
 		if (minDeltaMantissa > 5) {
@@ -233,17 +292,18 @@ public class GraphAxis {
 		} else {
 			actualDeltaMantissa = 1;
 		}
+
 		return minDelta * (actualDeltaMantissa / minDeltaMantissa);
 	}
 
 	public double getWidth() {
 		return width;
 	}
-	
+
 	public boolean contains(Vector2 pos) {
 		return bounds.contains(pos);
 	}
-	
+
 	protected void uncheckedTranslate(double motion) {
 		this.min += motion;
 		this.max += motion;
@@ -253,15 +313,15 @@ public class GraphAxis {
 		// First, try to translate to put in range
 		uncheckedTranslate(Math.max(0, minRange - this.min));
 		uncheckedTranslate(Math.min(0, maxRange - this.max));
-		
+
 		// Second, truncate to range
 		this.min = Math.max(this.min, minRange);
 		this.max = Math.min(this.max, maxRange);
 	}
-	
+
 	public void zoom(double factor, double about) {
-		this.min = about + factor * (this.min-about);
-		this.max = about + factor * (this.max-about);
+		this.min = about + factor * (this.min - about);
+		this.max = about + factor * (this.max - about);
 		clampToRange();
 		rescale();
 	}
--- src/org/bodytrack/client/GraphWidget.java
+++ src/org/bodytrack/client/GraphWidget.java
@@ -15,52 +15,69 @@ import gwt.g2d.client.graphics.Surface;
 import gwt.g2d.client.math.Vector2;
 
 public class GraphWidget extends Surface {
-	
+
 	private ArrayList<GraphAxis> xAxes = new ArrayList<GraphAxis>();
 	private ArrayList<GraphAxis> yAxes = new ArrayList<GraphAxis>();
 	private ArrayList<GraphAxis> allAxes = new ArrayList<GraphAxis>();
-	
+
 	private int width, height;
 	private int axisMargin;
 	private int graphMargin = 5;
 
 	private int graphWidth, graphHeight;
-	
+
+	private GraphAxis mouseDragAxis;
+	private Vector2 mouseDragLastPos;
+
 	GraphWidget(int width, int height, int axisMargin) {
 		super(width, height);
 		this.width = width;
 		this.height = height;
 		this.axisMargin = axisMargin;
-		
+
 		this.addMouseWheelHandler(new MouseWheelHandler() {
-			public void onMouseWheel(MouseWheelEvent event) { handleMouseWheelEvent(event); }
+			@Override
+			public void onMouseWheel(MouseWheelEvent event) {
+				handleMouseWheelEvent(event);
+			}
 		});
-	
+
 		this.addMouseDownHandler(new MouseDownHandler() {
-			public void onMouseDown(MouseDownEvent event) { handleMouseDownEvent(event); }
+			@Override
+			public void onMouseDown(MouseDownEvent event) {
+				handleMouseDownEvent(event);
+			}
 		});
 
 		this.addMouseMoveHandler(new MouseMoveHandler() {
-			public void onMouseMove(MouseMoveEvent event) { handleMouseMoveEvent(event); }
-
+			@Override
+			public void onMouseMove(MouseMoveEvent event) {
+				handleMouseMoveEvent(event);
+			}
 		});
 
 		this.addMouseUpHandler(new MouseUpHandler() {
-			public void onMouseUp(MouseUpEvent event) { handleMouseUpEvent(event); }
-		});		
-		
+			public void onMouseUp(MouseUpEvent event) {
+				handleMouseUpEvent(event);
+			}
+		});
+
 		this.addMouseOutHandler(new MouseOutHandler() {
-			public void onMouseOut(MouseOutEvent event) { handleMouseOutEvent(event); }
+			public void onMouseOut(MouseOutEvent event) {
+				handleMouseOutEvent(event);
+			}
 		});
 	}
 
 	GraphAxis findAxis(Vector2 pos) {
 		for (int i=0; i < allAxes.size(); i++) {
-			if (allAxes.get(i).contains(pos)) return allAxes.get(i);
+			if (allAxes.get(i).contains(pos))
+				return allAxes.get(i);
 		}
+
 		return null;
 	}
-	
+
 	private void handleMouseWheelEvent(MouseWheelEvent event) {
 		Vector2 eventLoc = new Vector2(event.getX(), event.getY());
 		GraphAxis axis = findAxis(eventLoc);
@@ -72,18 +89,16 @@ public class GraphWidget extends Surface {
 		}
 	}
 
-	private GraphAxis mouseDragAxis;
-	private Vector2 mouseDragLastPos;
-	
 	private void handleMouseDownEvent(MouseDownEvent event) {
 		Vector2 pos = new Vector2(event.getX(), event.getY());
 		GraphAxis axis = findAxis(pos);
+
 		if (axis != null) {
 			mouseDragAxis = axis;
 			mouseDragLastPos = pos;
 		}
 	}
-	
+
 	private void handleMouseMoveEvent(MouseMoveEvent event) {
 		Vector2 pos = new Vector2(event.getX(), event.getY());
 		if (mouseDragAxis != null) {
@@ -110,28 +125,37 @@ public class GraphWidget extends Surface {
 		int yAxesWidth = calculateAxesWidth(yAxes);
 		graphWidth = width - graphMargin - yAxesWidth;
 		graphHeight = height - graphMargin - xAxesWidth;
-		Vector2 xAxesBegin = new Vector2(graphMargin, graphHeight+graphMargin);
+		Vector2 xAxesBegin = new Vector2(graphMargin,
+			graphHeight + graphMargin);
 		layoutAxes(xAxes, graphWidth, xAxesBegin, Basis.xDownYRight);
-		
-		Vector2 yAxesBegin = new Vector2(graphWidth+graphMargin, graphHeight+graphMargin);
+
+		Vector2 yAxesBegin = new Vector2(graphWidth+graphMargin,
+			graphHeight + graphMargin);
 		layoutAxes(yAxes, graphHeight, yAxesBegin, Basis.xRightYUp);
 	}
-	
-	
+
 	private int calculateAxesWidth(ArrayList<GraphAxis> axes) {
 		int ret = 0;
+
 		for (int i=0; i < axes.size(); i++) {
 			ret += axes.get(i).getWidth();
-			if (i > 0) ret += axisMargin;
+
+			if (i > 0)
+				ret += axisMargin;
 		}
+
 		return ret;
 	}
-	
-	private void layoutAxes(ArrayList<GraphAxis> axes, double length, Vector2 begin, Basis basis) {
+
+	private void layoutAxes(ArrayList<GraphAxis> axes, double length,
+			Vector2 begin, Basis basis) {
 		Vector2 offset = begin;
+
 		for (int i=0; i < axes.size(); i++) {
 			axes.get(i).layout(offset, length);
-			offset = offset.add(basis.x.scale(axisMargin + axes.get(i).getWidth()));
+
+			offset = offset.add(
+					basis.x.scale(axisMargin + axes.get(i).getWidth()));
 		}
 	}
 
@@ -139,21 +163,22 @@ public class GraphWidget extends Surface {
 		layout();
 		this.clear();
 		//this.clearRectangle(0,0,400,400);
-		//this.setFillStyle(new Color((int) (Random.nextDouble() * 255), 128, 128));
+		//this.setFillStyle(new Color(
+		//		(int) (Random.nextDouble() * 255), 128, 128));
 		//this.fillRectangle(0,0,500,500);
 		this.save();
 		this.translate(.5, .5);
 		for (int i=0; i < allAxes.size(); i++) allAxes.get(i).paint(this);
 		this.restore();
-		
+
 		//DirectShapeRenderer renderer = new DirectShapeRenderer(this);
-	    //renderer.beginPath();
-	    
-	    //for (double x = 0; x < 400; x++) {
-	    //	double y = 200+200*Math.sin(x*.1);
-	    //	renderer.drawLineTo(x,y);
-	    //}
-	    //renderer.stroke();
+		//renderer.beginPath();
+
+		//for (double x = 0; x < 400; x++) {
+		//	double y = 200+200*Math.sin(x*.1);
+		//	renderer.drawLineTo(x,y);
+		//}
+		//renderer.stroke();
 	}
 
 	public void addXAxis(GraphAxis graphAxis) {
