732d54d9bd611bf29f5de107f227428440ea39ae
Adam Mihalcin <adammih@gmail.com>
2010-10-20 12:25:31 -0400

    Made the Canvas class instance-controlled for efficiency.
    Now a factory method only creates a new Canvas (and associated
    DirectShapeRenderer) if a Canvas has not already been created for the
    given surface.

--- src/org/bodytrack/client/Canvas.java
+++ src/org/bodytrack/client/Canvas.java
@@ -1,31 +1,64 @@
 package org.bodytrack.client;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import gwt.g2d.client.graphics.DirectShapeRenderer;
 import gwt.g2d.client.graphics.Surface;
 
 /**
  * Wrapper for a G2D Surface/DirectShapeRenderer pair.
  *
- * This is intended to collect many of the important methods of the
+ * <p>This is intended to collect many of the important methods of the
  * {@link gwt.g2d.client.graphics.Surface Surface} and its associated
  * {@link gwt.g2d.client.graphics.DirectShapeRenderer DirectShapeRenderer}
  * in one class.  Note that not all methods of the two classes are
  * represented here.  However, the important methods of the two classes are
  * wrapped here, and calls to {@link #getSurface()} and to
  * {@link #getRenderer()}, which simply return references to those two
- * objects, allow calls to the other methods.
+ * objects, allow calls to the other methods.</p>
+ *
+ * <p>This class is instance-controlled for efficiency: under this
+ * system, only one DirectShapeRenderer is created per Surface.</p>
  */
 public class Canvas {
 	private Surface surface;
 	private DirectShapeRenderer renderer;
 
-	public Canvas(Surface s) {
+	private static Map<Surface, Canvas> instances;
+
+	static {
+		instances = new HashMap<Surface, Canvas>();
+	}
+
+	private Canvas() { }
+
+	/**
+	 * Factory method to create a new Canvas object.
+	 *
+	 * @param s
+	 * 		the {@link gwt.g2d.client.graphics.Surface Surface}
+	 * 		on which the new Canvas will draw
+	 * @return
+	 * 		a Canvas with a pointer to s and to an associated
+	 * 		{@link gwt.g2d.client.graphics.DirectShapeRenderer
+	 * 		DirectShapeRenderer}
+	 */
+	public static Canvas buildCanvas(Surface s) {
 		if (s == null)
 			throw new NullPointerException("Surface cannot be null");
 
-		surface = s;
+		// We have already made a Canvas that points to s
+		if (instances.containsKey(s))
+			return instances.get(s);
 
-		renderer = new DirectShapeRenderer(surface);
+		Canvas result = new Canvas();
+
+		result.surface = s;
+
+		result.renderer = new DirectShapeRenderer(result.surface);
+
+		return result;
 	}
 
 	/**
