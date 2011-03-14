2c666600f9f52b5ac3ac8fe15767a319b0ce0ee6
Adam Mihalcin <adammih@gmail.com>
2011-03-14 14:34:26 -0400

    Moved the StringPair class from ChannelNamesWidget to ChannelManager
    This was a simple refactoring, since the StringPair class stands on its
    own but is used as a static class.

--- src/org/bodytrack/client/ChannelNamesWidget.java
+++ src/org/bodytrack/client/ChannelNamesWidget.java
@@ -8,6 +8,8 @@ import java.util.Map;
 import java.util.Set;
 import java.util.SortedMap;
 
+import org.bodytrack.client.ChannelManager.StringPair;
+
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.event.shared.HandlerRegistration;
@@ -198,81 +200,6 @@ public class ChannelNamesWidget extends VerticalPanel {
 			else
 				// User just unchecked a button
 				listener.channelRemoved(deviceName, channelName);
-		}
-	}
-
-	/**
-	 * A simple class to hold an immutable pair of non-<tt>null</tt> strings.
-	 */
-	private static class StringPair {
-		private String s1;
-		private String s2;
-
-		/**
-		 * Initializes a new <tt>StringPair</tt>.
-		 *
-		 * @param s1
-		 * 		the first string to hold
-		 * @param s2
-		 * 		the second string to hold
-		 */
-		public StringPair(String s1, String s2) {
-			if (s1 == null || s2 == null)
-				throw new NullPointerException(
-					"Cannot use null string with the StringPair class");
-
-			this.s1 = s1;
-			this.s2 = s2;
-		}
-
-		/**
-		 * Returns the first string this holds.
-		 *
-		 * @return
-		 * 		the value of s1 passed to the constructor when this
-		 * 		<tt>StringPair</tt> was created
-		 */
-		public String getFirst() {
-			return s1;
-		}
-
-		/**
-		 * Returns the second string this holds.
-		 *
-		 * @return
-		 * 		the value of s2 passed to the constructor when this
-		 * 		<tt>StringPair</tt> was created
-		 */
-		public String getSecond() {
-			return s2;
-		}
-
-		/**
-		 * @see java.lang.Object#hashCode()
-		 */
-		@Override
-		public int hashCode() {
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + s1.hashCode();
-			result = prime * result + s2.hashCode();
-			return result;
-		}
-
-		/**
-		 * @see java.lang.Object#equals(java.lang.Object)
-		 */
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (! (obj instanceof StringPair))
-				return false;
-			StringPair other = (StringPair) obj;
-			// We know that s1 and s2 are never null
-			return s1.equals(other.s1) && s2.equals(other.s2);
 		}
 	}
 }
