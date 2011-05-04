4e4ced783bc58423f109c2a83980ef537086cdba
Adam Mihalcin <adammih@gmail.com>
2011-05-04 12:55:58 -0400

    Added a short comment to Alertable
    This just says that there are too many interfaces in the package that
    are all intended to enable continuation-passing style, and that the
    number needs to decrease.

--- src/org/bodytrack/client/Alertable.java
+++ src/org/bodytrack/client/Alertable.java
@@ -7,6 +7,11 @@ package org.bodytrack.client;
  * 		the types of messages that will be sent to objects whenever
  * 		events occur
  */
+// TODO: There are at least four interfaces (this interface,
+// Continuation, WebDownloader.DownloadAlertable, and
+// WebDownloader.DownloadSuccessAlertable) that all try to allow CPS
+// to work in Java, since Java doesn't have lambdas or even delegates.
+// I need to cut the number of interfaces down to one or two
 public interface Alertable<T> {
 	/**
 	 * Called whenever a successful action occurs.
