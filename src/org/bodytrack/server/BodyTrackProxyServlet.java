e75085307bc26a0c44c09210bcf258fde3da1804
rsargent <randy.sargent@gmail.com>
2011-07-10 07:47:08 -0400

    Proxy server sort-of-works.  Compiles and runs under GWT 2.3

---
+++ src/org/bodytrack/server/BodyTrackProxyServlet.java
@@ -0,0 +1,28 @@
+package org.bodytrack.server;
+
+import java.io.IOException;
+import java.net.URL;
+
+//import javax.naming.ConfigurationException;
+import javax.servlet.ServletException;
+
+import com.woonoz.proxy.servlet.ProxyServlet;
+
+public class BodyTrackProxyServlet extends ProxyServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+    public void init() throws ServletException {
+            try {
+            	URL url = new URL("http://bodytrack.org/");
+            init(url, 200);
+            } catch (IOException e) {
+                    throw new ServletException(e);
+            }// catch (ConfigurationException e) {
+             //       throw new ServletException(e);
+             //}
+    }       
+}
