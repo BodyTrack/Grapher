5dd5b43ac8be388b6f6e447bf31fdfc5b9ed4893
Adam Mihalcin <adammih@gmail.com>
2011-10-14 14:27:07 -0400

    Refactored the NumberAxis and DateAxis code to use a single constructor
    This approach takes advantage of JavaScript's first-class functions to
    create a function called __createAxisConstructor that takes a function
    representing the Java constructor and returns a the JavaScript
    constructor to wrap that Java constructor.

--- src/org/bodytrack/client/InfoPublisher.java
+++ src/org/bodytrack/client/InfoPublisher.java
@@ -13,6 +13,81 @@ public final class InfoPublisher {
 	 * Initializes the wrapper objects for the JavaScript API.
 	 */
 	public static native void setUpWrappers() /*-{
+		/// Private function to create a new axis constructor
+		///
+		/// This is intended to be called only to create the NumberAxis and
+		/// GraphAxis constructors.  It is declared as a var so that its
+		/// scope will be local to the function that GWT creates to replace
+		/// setUpWrappers.
+		var __createAxisConstructor = function(axisConstructor) {
+			return function(placeholder, orientation, range) {
+				if (placeholder === undefined || orientation === undefined) {
+					throw 'Must pass in placeholder and orientation';
+				}
+
+				if (orientation != 'horizontal' && orientation != 'vertical') {
+					throw 'Orientation must be either "horizontal" or "vertical"';
+				}
+
+				if (!range) {
+					range = {
+						'min': -10,
+						'max': 10
+					};
+				} else {
+					if (!('min' in range)) {
+						range.min = -10;
+					}
+					if (!('max' in range)) {
+						range.max = 10;
+					}
+				}
+
+				this.getPlaceholder = function() { return placeholder; };
+				this.getOrientation = function() { return orientation; };
+				this.__backingAxis = function() {
+					// Call an anonymous function to avoid later closures
+					// keeping around any objects longer than necessary
+
+					// Setup code
+					var DEFAULT_WIDTH = 10;
+					var placeholderElement = placeholder !== null
+						? $doc.getElementById(placeholder)
+						: null;
+
+					var axisMin = range.min;
+					var axisMax = range.max;
+					var isXAxis = orientation == 'horizontal';
+
+					var axisWidth = null;
+					var basis = null;
+					if (isXAxis) {
+						axisWidth = placeholderElement !== null
+							? placeholderElement.offsetHeight
+							: DEFAULT_WIDTH;
+						basis = @org.bodytrack.client.Basis::xDownYRight;
+					} else {
+						axisWidth = placeholderElement !== null
+							? placeholderElement.offsetWidth
+							: DEFAULT_WIDTH;
+						basis = @org.bodytrack.client.Basis::xRightYUp;
+					}
+
+					return axisConstructor(placeholder, axisMin, axisMax, basis, axisWidth, isXAxis);
+				}();
+				this.getMin = function() {
+					return this.__backingAxis.@org.bodytrack.client.GraphAxis::getMin()();
+				};
+				this.getMax = function() {
+					return this.__backingAxis.@org.bodytrack.client.GraphAxis::getMax()();
+				};
+				this.setRange = function(min, max) {
+					// Exceptions for illegal min and max values are handled in Java
+					this.__backingAxis.@org.bodytrack.client.GraphAxis::replaceBounds(DD)(min, max);
+				};
+			};
+		}
+
 		/// Initializes a new NumberAxis object
 		///
 		/// @param placeholder
@@ -24,72 +99,7 @@ public final class InfoPublisher {
 		///		representing the bounds on the axis.  The values on these keys
 		///		must be numbers, with the max value greater than the min value.
 		///		If this key is not present, a default range will be assigned
-		function NumberAxis(placeholder, orientation, range) {
-			if (placeholder === undefined || orientation === undefined) {
-				throw 'Must pass in placeholder and orientation';
-			}
-
-			if (orientation != 'horizontal' && orientation != 'vertical') {
-				throw 'Orientation must be either "horizontal" or "vertical"';
-			}
-
-			if (!range) {
-				range = {
-					'min': -10,
-					'max': 10
-				};
-			} else {
-				if (!('min' in range)) {
-					range.min = -10;
-				}
-				if (!('max' in range)) {
-					range.max = 10;
-				}
-			}
-
-			this.getPlaceholder = function() { return placeholder; };
-			this.getOrientation = function() { return orientation; };
-			this.__backingAxis = function() {
-				// Call an anonymous function to avoid later closures
-				// keeping around any objects longer than necessary
-
-				// Setup code
-				var DEFAULT_WIDTH = 10;
-				var placeholderElement = placeholder !== null
-					? $doc.getElementById(placeholder)
-					: null;
-
-				var axisMin = range.min;
-				var axisMax = range.max;
-				var isXAxis = orientation == 'horizontal';
-
-				var axisWidth = null;
-				var basis = null;
-				if (isXAxis) {
-					axisWidth = placeholderElement !== null
-						? placeholderElement.offsetHeight
-						: DEFAULT_WIDTH;
-					basis = @org.bodytrack.client.Basis::xDownYRight;
-				} else {
-					axisWidth = placeholderElement !== null
-						? placeholderElement.offsetWidth
-						: DEFAULT_WIDTH;
-					basis = @org.bodytrack.client.Basis::xRightYUp;
-				}
-
-				return @org.bodytrack.client.GraphAxis::new(Ljava/lang/String;DDLorg/bodytrack/client/Basis;DZ)(placeholder, axisMin, axisMax, basis, axisWidth, isXAxis);
-			}();
-			this.getMin = function() {
-				return this.__backingAxis.@org.bodytrack.client.GraphAxis::getMin()();
-			};
-			this.getMax = function() {
-				return this.__backingAxis.@org.bodytrack.client.GraphAxis::getMax()();
-			};
-			this.setRange = function(min, max) {
-				// Exceptions for illegal min and max values are handled in Java
-				this.__backingAxis.@org.bodytrack.client.GraphAxis::replaceBounds(DD)(min, max);
-			};
-		}
+		$wnd.NumberAxis = __createAxisConstructor(@org.bodytrack.client.GraphAxis::new(Ljava/lang/String;DDLorg/bodytrack/client/Basis;DZ));
 
 		/// Initializes a new DateAxis object
 		///
@@ -102,71 +112,6 @@ public final class InfoPublisher {
 		///		representing the bounds on the axis.  The values on these keys
 		///		must be numbers, with the max value greater than the min value.
 		///		If this key is not present, a default range will be assigned
-		function DateAxis(placeholder, orientation, range) {
-			if (placeholder === undefined || orientation === undefined) {
-				throw 'Must pass in placeholder and orientation';
-			}
-
-			if (orientation != 'horizontal' && orientation != 'vertical') {
-				throw 'Orientation must be either "horizontal" or "vertical"';
-			}
-
-			if (!range) {
-				range = {
-					'min': -10,
-					'max': 10
-				};
-			} else {
-				if (!('min' in range)) {
-					range.min = -10;
-				}
-				if (!('max' in range)) {
-					range.max = 10;
-				}
-			}
-
-			this.getPlaceholder = function() { return placeholder; };
-			this.getOrientation = function() { return orientation; };
-			this.__backingAxis = function() {
-				// Call an anonymous function to avoid later closures
-				// keeping around any objects longer than necessary
-
-				// Setup code
-				var DEFAULT_WIDTH = 10;
-				var placeholderElement = placeholder !== null
-					? $doc.getElementById(placeholder)
-					: null;
-
-				var axisMin = range.min;
-				var axisMax = range.max;
-				var isXAxis = orientation == 'horizontal';
-
-				var axisWidth = null;
-				var basis = null;
-				if (isXAxis) {
-					axisWidth = placeholderElement !== null
-						? placeholderElement.offsetHeight
-						: DEFAULT_WIDTH;
-					basis = @org.bodytrack.client.Basis::xDownYRight;
-				} else {
-					axisWidth = placeholderElement !== null
-						? placeholderElement.offsetWidth
-						: DEFAULT_WIDTH;
-					basis = @org.bodytrack.client.Basis::xRightYUp;
-				}
-
-				return @org.bodytrack.client.TimeGraphAxis::new(Ljava/lang/String;DDLorg/bodytrack/client/Basis;DZ)(placeholder, axisMin, axisMax, basis, axisWidth, isXAxis);
-			}();
-			this.getMin = function() {
-				return this.__backingAxis.@org.bodytrack.client.TimeGraphAxis::getMin()();
-			};
-			this.getMax = function() {
-				return this.__backingAxis.@org.bodytrack.client.TimeGraphAxis::getMax()();
-			};
-			this.setRange = function(min, max) {
-				// Exceptions for illegal min and max values are handled in Java
-				this.__backingAxis.@org.bodytrack.client.TimeGraphAxis::replaceBounds(DD)(min, max);
-			};
-		}
+		$wnd.DateAxis = __createAxisConstructor(@org.bodytrack.client.TimeGraphAxis::new(Ljava/lang/String;DDLorg/bodytrack/client/Basis;DZ));
 	}-*/;
 }
