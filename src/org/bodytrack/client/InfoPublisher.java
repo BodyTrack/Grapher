1e2e8adefab8502b165b3668181be47d46645b75
Adam Mihalcin <adammih@gmail.com>
2011-10-14 13:43:31 -0400

    Added the DateAxis class to the JavaScript API
    Like the NumberAxis class, this is untested.  Also, this is very
    similar to the NumberAxis code, differing only in the type of the
    backing object.

--- src/org/bodytrack/client/InfoPublisher.java
+++ src/org/bodytrack/client/InfoPublisher.java
@@ -90,5 +90,83 @@ public final class InfoPublisher {
 				this.__backingAxis.@org.bodytrack.client.GraphAxis::replaceBounds(DD)(min, max);
 			};
 		}
+
+		/// Initializes a new DateAxis object
+		///
+		/// @param placeholder
+		///		The ID of a div in which this axis should go, or null
+		/// @param orientation
+		///		Either 'horizontal', for an X-axis, or 'vertical', for a Y-axis
+		/// @param range
+		///		Optional parameter: a dictionary with keys 'min' and 'max',
+		///		representing the bounds on the axis.  The values on these keys
+		///		must be numbers, with the max value greater than the min value.
+		///		If this key is not present, a default range will be assigned
+		function DateAxis(placeholder, orientation, range) {
+			if (placeholder === undefined || orientation === undefined) {
+				throw 'Must pass in placeholder and orientation';
+			}
+
+			if (orientation != 'horizontal' && orientation != 'vertical') {
+				throw 'Orientation must be either "horizontal" or "vertical"';
+			}
+
+			if (!range) {
+				range = {
+					'min': -10,
+					'max': 10
+				};
+			} else {
+				if (!('min' in range)) {
+					range.min = -10;
+				}
+				if (!('max' in range)) {
+					range.max = 10;
+				}
+			}
+
+			this.getPlaceholder = function() { return placeholder; };
+			this.getOrientation = function() { return orientation; };
+			this.__backingAxis = function() {
+				// Call an anonymous function to avoid later closures
+				// keeping around any objects longer than necessary
+
+				// Setup code
+				var DEFAULT_WIDTH = 10;
+				var placeholderElement = placeholder !== null
+					? $doc.getElementById(placeholder)
+					: null;
+
+				var axisMin = range.min;
+				var axisMax = range.max;
+				var isXAxis = orientation == 'horizontal';
+
+				var axisWidth = null;
+				var basis = null;
+				if (isXAxis) {
+					axisWidth = placeholderElement !== null
+						? placeholderElement.offsetHeight
+						: DEFAULT_WIDTH;
+					basis = @org.bodytrack.client.Basis::xDownYRight;
+				} else {
+					axisWidth = placeholderElement !== null
+						? placeholderElement.offsetWidth
+						: DEFAULT_WIDTH;
+					basis = @org.bodytrack.client.Basis::xRightYUp;
+				}
+
+				return @org.bodytrack.client.TimeGraphAxis::new(Ljava/lang/String;DDLorg/bodytrack/client/Basis;DZ)(placeholder, axisMin, axisMax, basis, axisWidth, isXAxis);
+			}();
+			this.getMin = function() {
+				return this.__backingAxis.@org.bodytrack.client.TimeGraphAxis::getMin()();
+			};
+			this.getMax = function() {
+				return this.__backingAxis.@org.bodytrack.client.TimeGraphAxis::getMax()();
+			};
+			this.setRange = function(min, max) {
+				// Exceptions for illegal min and max values are handled in Java
+				this.__backingAxis.@org.bodytrack.client.TimeGraphAxis::replaceBounds(DD)(min, max);
+			};
+		}
 	}-*/;
 }
