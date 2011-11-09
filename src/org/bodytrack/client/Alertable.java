85211cdb945288bdc249f7ec478e4414751f87e0
Chris Bartley <bartley@cmu.edu>
2011-11-09 17:27:45 -0500

    Removed unused classes

--- src/org/bodytrack/client/Alertable.java
+++ src/org/bodytrack/client/Alertable.java
@@ -7,13 +7,6 @@ package org.bodytrack.client;
  * 		the types of messages that will be sent to objects whenever
  * 		events occur
  */
-// TODO: There are at least four interfaces (this interface,
-// Continuation, WebDownloader.DownloadAlertable, and
-// WebDownloader.DownloadSuccessAlertable) that all try to allow CPS
-// to work in Java, since Java doesn't have lambdas or even delegates.
-// I need to cut the number of interfaces down to one or two.  Probably
-// one for the success/failure case, one for the success case, and maybe
-// one for the "alert when I'm done" case
 public interface Alertable<T> {
 	/**
 	 * Called whenever a successful action occurs.
--- src/org/bodytrack/client/G.java
+++
@@ -1,6 +0,0 @@
-package org.bodytrack.client;
-
-public class G {
-	static int user_id;
-	static String username;
-}
