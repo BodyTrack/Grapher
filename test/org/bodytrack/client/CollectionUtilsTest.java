721079a975590c87a797cbfe101d14851f9fa24a
Adam Mihalcin <adammih@gmail.com>
2012-01-03 15:57:57 -0500

    Added a more stressful test for CollectionUtils.insertInOrder
    All the tests stored in CollectionUtilsTest.java pass.

--- test/org/bodytrack/client/CollectionUtilsTest.java
+++ test/org/bodytrack/client/CollectionUtilsTest.java
@@ -3,11 +3,14 @@ package org.bodytrack.client;
 import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 import org.junit.Test;
 
 public class CollectionUtilsTest {
+	private static final int SEED = 424242;
 
 	@Test(expected=NullPointerException.class)
 	public void testNullList() {
@@ -44,5 +47,25 @@ public class CollectionUtilsTest {
 		assertEquals(lst.get(0).intValue(), 5);
 		assertEquals(lst.get(1).intValue(), 7);
 		assertEquals(lst.get(2).intValue(), 42);
+	}
+
+	@Test
+	public void testLongList() {
+		final List<Integer> lst = new ArrayList<Integer>();
+		final List<Integer> copy = new ArrayList<Integer>();
+		final Random rg = new Random(SEED);
+
+		for (int i = 0; i < 10000; i++) {
+			int n = rg.nextInt();
+			lst.add(n);
+			CollectionUtils.insertInOrder(copy, n);
+		}
+
+		Collections.sort(lst);
+
+		assertEquals(lst.size(), copy.size());
+
+		for (int i = 0; i < lst.size(); i++)
+			assertEquals(lst.get(i), copy.get(i));
 	}
 }
