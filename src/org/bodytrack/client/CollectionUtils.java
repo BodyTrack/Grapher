8f64336eb2f2ac2f80212bc01a8bf8128a6dd246
Adam Mihalcin <adammih@gmail.com>
2012-01-11 12:23:56 -0500

    Added a getFirst method to CollectionUtils
    This just returns the first item returned by the argument's iterator.

--- src/org/bodytrack/client/CollectionUtils.java
+++ src/org/bodytrack/client/CollectionUtils.java
@@ -1,5 +1,6 @@
 package org.bodytrack.client;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -8,7 +9,8 @@ public final class CollectionUtils {
 		throw new UnsupportedOperationException();
 	}
 
-	public static <T extends Comparable<T>> void insertInOrder(List<T> lst, T elem) {
+	public static <T extends Comparable<T>> void insertInOrder(final List<T> lst,
+			final T elem) {
 		if (lst == null || elem == null)
 			throw new NullPointerException();
 
@@ -17,7 +19,7 @@ public final class CollectionUtils {
 			return;
 		}
 
-		int idx = Collections.binarySearch(lst, elem);
+		final int idx = Collections.binarySearch(lst, elem);
 
 		if (idx < 0) {
 			// Collections.binarySearch returns (-(insertion point) - 1) if
@@ -26,5 +28,22 @@ public final class CollectionUtils {
 		} else {
 			lst.add(idx, elem);
 		}
+	}
+
+	/**
+	 * Returns the first item in lst
+	 *
+	 * @param <T>
+	 * 	The type of object that this method should return
+	 * @param lst
+	 * 	A collection of objects
+	 * @return
+	 * 	The first item returned by the iterator for lst, or
+	 * 	<code>null</code> if lst is empty
+	 */
+	public static <T> T getFirst(final Collection<T> lst) {
+		for (final T value: lst)
+			return value;
+		return null;
 	}
 }
