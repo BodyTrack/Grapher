fff982e17a8609cfc864dbf9e901923819392511
Adam Mihalcin <adammih@gmail.com>
2012-01-18 10:55:22 -0500

    Added an info field to the target parameter for a DataPointListener

--- src/org/bodytrack/client/DataPointListener.java
+++ src/org/bodytrack/client/DataPointListener.java
@@ -24,31 +24,41 @@ public final class DataPointListener extends JavaScriptObject {
 
 	protected DataPointListener() { }
 
+	public void handleDataPointUpdate(final PlottablePoint point,
+			final TriggerAction action) {
+		handleDataPointUpdate(point, action, null);
+	}
+
 	/**
-	 * The dispatcher helper function that determines which native call to make
+	 * Fires the callback with the specified information
 	 *
 	 * @param point
 	 * 	The point (may be <code>null</code>) about which the page needs to
 	 * 	be informed
 	 * @param action
-	 * 	The non-<code>null</code> action that triggered this call
+	 * 	The action that triggered this call
+	 * @param info
+	 * 	An JavaScript object that, if not null, gets passed in as part of
+	 * 	the target argument to the callback
 	 * @throws NullPointerException
 	 * 	If action is <code>null</code>
 	 */
 	public void handleDataPointUpdate(final PlottablePoint point,
-			final TriggerAction action) {
+			final TriggerAction action,
+			final JavaScriptObject info) {
 		if (action == null)
 			throw new NullPointerException();
 
 		if (point == null)
-			handleNoDataPointUpdate(action.getActionName());
+			handleNoDataPointUpdate(action.getActionName(), info);
 		else
 			handleDataPointUpdate(point.getDate(),
 					point.getValue(),
 					point.getDateAsString(),
 					point.getValueAsString(),
 					point.getComment(),
-					action.getActionName());
+					action.getActionName(),
+					info);
 	}
 
 	private native void handleDataPointUpdate(final double date,
@@ -56,23 +66,43 @@ public final class DataPointListener extends JavaScriptObject {
 			final String dateStr,
 			final String valueStr,
 			final String comment,
-			final String actionName) /*-{
-		this({
-				"date": date,
-				"value": value,
-				"dateString": dateStr,
-				"valueString": valueStr,
-				"comment": comment
-			},
-			{
-				"actionName": actionName
-			});
+			final String actionName,
+			final JavaScriptObject info) /*-{
+		var pointObj = {
+			"date": date,
+			"value": value,
+			"dateString": dateStr,
+			"valueString": valueStr,
+			"comment": comment
+		};
+
+		if (info == null) {
+			this(pointObj,
+				{
+					"actionName": actionName
+				});
+		} else {
+			this(pointObj,
+				{
+					"actionName": actionName,
+					"info": info
+				});
+		}
 	}-*/;
 
-	private native void handleNoDataPointUpdate(final String actionName) /*-{
-		this(null,
+	private native void handleNoDataPointUpdate(final String actionName,
+			final JavaScriptObject info) /*-{
+		if (info == null) {
+			this(null,
 			{
 				"actionName": actionName
 			});
+		} else {
+			this(null,
+			{
+				"actionName": actionName,
+				"info": info
+			});
+		}
 	}-*/;
 }
