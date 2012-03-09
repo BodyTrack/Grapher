3746c845c3c43b276d3672193974d4d9748583fa
Adam Mihalcin <adammih@gmail.com>
2012-03-09 15:31:39 -0500

    Added the capability for throttled sequence numbers
    These sequence numbers only get updated once every 100 milliseconds.

--- src/org/bodytrack/client/SequenceNumber.java
+++ src/org/bodytrack/client/SequenceNumber.java
@@ -1,26 +1,72 @@
 package org.bodytrack.client;
 
+import java.util.Date;
+
 /**
  * <p>
- * <code>SequenceNumber</code> produces an <code>int</code> sequence number, starting at 1.  The sequence number resets
- * to 1 after reaching {@link Integer#MAX_VALUE}.
+ * {@link #getNext()} produces an <code>int</code> sequence number,
+ * starting at 1.  The sequence number resets to 1 after reaching
+ * {@link #MIN_THROTTLED}.
+ * </p>
+ *
+ * <p>
+ * This segments the set of integers into two regions - one for regular
+ * sequence numbers and one for throttled sequence numbers.  Regular
+ * sequence numbers go from 1 to {@link #MIN_THROTTLED}, and throttled
+ * sequence numbers go from <code>MIN_THROTTLED + 1</code> to
+ * {@link Integer#MAX_VALUE}.
  * </p>
  *
  * @author Chris Bartley (bartley@cmu.edu)
  */
 public class SequenceNumber {
-   private static final SequenceNumber INSTANCE = new SequenceNumber();
+	private static final SequenceNumber INSTANCE = new SequenceNumber();
 
-   public static int getNext() {
-      if (INSTANCE.id == Integer.MAX_VALUE) {
-         INSTANCE.id = 0;
-      }
-      return ++INSTANCE.id;
-   }
+	// The number of milliseconds between unique values returned from
+	// getNextThrottled
+	private static final int THROTTLED_THRESHOLD = 100;
 
-   private int id = 0;
+	// Have to divide and then multiple in order to prevent overflow
+	private static final int MIN_THROTTLED = (Integer.MAX_VALUE / 4) * 3;
 
-   private SequenceNumber() {
-      // private to prevent instantiation
-   }
+	public static int getNext() {
+		if (INSTANCE.id == MIN_THROTTLED) {
+			INSTANCE.id = 0;
+		}
+		return ++INSTANCE.id;
+	}
+
+	/**
+	 * Similar to {@link #getNext()}, but only returns a new value once every
+	 * {@link #THROTTLED_THRESHOLD} milliseconds.
+	 *
+	 * @return
+	 * 	A new value if and only if there has been at least
+	 * 	{@link #THROTTLED_THRESHOLD} milliseconds since the last new value
+	 */
+	public static int getNextThrottled() {
+		long now = new Date().getTime();
+
+		if (now - INSTANCE.latestThrottledTimestamp >= THROTTLED_THRESHOLD) {
+			INSTANCE.latestThrottledTimestamp = now;
+
+			// Need to return a new ID
+			if (INSTANCE.throttledId == Integer.MAX_VALUE) {
+				INSTANCE.throttledId = MIN_THROTTLED;
+			}
+			return ++INSTANCE.throttledId;
+		}
+
+		// Return the old ID because THROTTLED_THRESHOLD milliseconds
+		// haven't passed
+		return INSTANCE.throttledId;
+	}
+
+	private int id = 0;
+	private int throttledId = MIN_THROTTLED;
+	private long latestThrottledTimestamp = 0;
+
+	private SequenceNumber() {
+		// private to prevent instantiation
+	}
 }
