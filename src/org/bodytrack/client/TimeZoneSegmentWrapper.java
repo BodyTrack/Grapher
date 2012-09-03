0a6d259a33e93d2ca6f658cf22331921c2bd073d
Adam Mihalcin <adammih@gmail.com>
2012-09-03 12:04:40 -0400

    Changed TimeZoneSegment and TimeZoneSegmentWrapper to Unix line endings
    Also changed the style in these two files to place a space before an
    opening curly brace.

--- src/org/bodytrack/client/TimeZoneSegmentWrapper.java
+++ src/org/bodytrack/client/TimeZoneSegmentWrapper.java
@@ -5,41 +5,41 @@ import com.google.gwt.i18n.client.TimeZone;
 public class TimeZoneSegmentWrapper {
 	private String name;
 	private boolean usesDST;
-	private double offset,start,end;
+	private double offset;
+	private double start;
+	private double end;
 	private TimeZone timeZone;
-	
-	public TimeZoneSegmentWrapper(TimeZoneSegment segment){
+
+	public TimeZoneSegmentWrapper(TimeZoneSegment segment) {
 		name = segment.getTimeZoneName();
 		usesDST = segment.usesDST();
 		offset = segment.getOffset();
 		start = segment.getStart();
 		end = segment.getEnd();
 		timeZone = segment.getTimeZone();
-		
-	}
-	
-	public String getTimeZoneName(){
-		return name;
-	}
-	
-	public double getOffset(){
-		return offset;
-	}
-	
-	public boolean usesDST(){
-		return usesDST;
-	}
-	
-	public double getStart(){
-		return start;
-	}
-	
-	public double getEnd(){
-		return end;
-	}
-	
-	public TimeZone getTimeZone(){
-		return timeZone;
 	}
 
+	public String getTimeZoneName() {
+		return name;
+	}
+
+	public double getOffset() {
+		return offset;
+	}
+
+	public boolean usesDST() {
+		return usesDST;
+	}
+
+	public double getStart() {
+		return start;
+	}
+
+	public double getEnd() {
+		return end;
+	}
+
+	public TimeZone getTimeZone() {
+		return timeZone;
+	}
 }
